d17a6d9c56d70e7fe730b3c0621093fb3799405f
Michał Muskała <michal@muskala.eu>
2020-12-02 00:03:27 +0000

    Complete setup

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+use aoc_runner_derive::aoc_lib;
+
+mod day1;
+
+aoc_lib! { year = 2020 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = aoc_2020 }
